fa8bd19ec32391a727f4ad60b50d07d6286698ee
Carl Cortright <ckcortright@gmail.com>
2018-03-04 12:31:38 -0700

    first pass at a team site

---
+++ _site/assets/js/events.js
@@ -0,0 +1,17 @@
+$(function(){
+
+	$('.notice-message').hide();
+
+	var events = $('.event');
+
+	events.each(function(i,event){
+		event = $(event);
+		var date = event.data('date');
+		var until = event.find('.until');
+		date = moment(date, "YYYY-MM-DD h:mm a");
+		until.html(date.fromNow());
+		if (date.isBefore(moment())){
+			event.parent().addClass('past');
+		}
+	})
+})
