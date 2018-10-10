d7aa39fd24697c95292ba15f469d28d19023aabe
devonsoto77 <deso8093@coalorado.edu>
2018-10-09 23:54:24 -0600

    changed startups2students event name

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-$( "span" ).each(function( index ) {
-    var eventName = $(this).text();
-    if(eventName === "Startups2Students") {
-        var newString = eventName.substring(0, 9) + "<br>" + eventName.substring(9);
-        $("span:contains('2')").replaceWith(newString);
-    }
-  });
-
